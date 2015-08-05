8a5ae120cf2e316746d6f7b48ef178febb2ffdf6
Randy Davis <randy_davis@outlook.com>
2015-08-04 23:04:42 -0500

    update customer server model

--- app/models/customer.server.model.js
+++ app/models/customer.server.model.js
@@ -10,10 +10,49 @@ var mongoose = require('mongoose'),
  * Customer Schema
  */
 var CustomerSchema = new Schema({
-	name: {
+	firstName: {
 		type: String,
 		default: '',
-		required: 'Please fill Customer name',
+		trim: true
+	},
+	lastName: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	suburb: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	country: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	industry: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	email: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	phone: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	referred: {
+		type: Boolean,
+		default: false,
+		trim: true
+	},
+	channel: {
+		type: String,
+		default: '',
 		trim: true
 	},
 	created: {
