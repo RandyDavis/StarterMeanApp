978eda632837afda44c339ebc844d2303e80a52c
Randy Davis <randy_davis@outlook.com>
2015-08-08 16:52:44 -0500

    save customer data with AngularJS Modal for update

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -20,6 +20,14 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', '$moda
 	      templateUrl: 'modules/customers/views/edit-customer.client.view.html',
 	      controller: function ($scope, $modalInstance, customer) {
 	      	$scope.customer = customer;
+
+	      	$scope.ok = function () {
+				    $modalInstance.close($scope.customer);
+				  };
+
+				  $scope.cancel = function () {
+				    $modalInstance.dismiss('cancel');
+				  };
 	      },
 	      size: size,
 	      resolve: {
@@ -49,7 +57,16 @@ customersApp.controller('CustomersCreateController', ['$scope', 'Customers', fun
 ]);
 
 customersApp.controller('CustomersUpdateController', ['$scope', 'Customers', function($scope, Customers) {
-	
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
+
+			customer.$update(function() {
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 	}
 ]);
 
